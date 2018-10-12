c8eb206bc87cb2370431f0c5886f84c23b8ca976
Yoonsub <yoonsub90@gmail.com>
2018-10-11 21:46:09 -0400

    delete function fixed

--- public/js/lists.js
+++ public/js/lists.js
@@ -1,34 +1,15 @@
 $(function () {
 
     const renderList = function () {
-        // $('.content').empty();
+        $('.lists').empty();
 
 
-        $.get("/api/lists")
+        $.ajax({url:"/api/lists",method:"GET"})
             .then(function (dataList) {
                 let contentHtml = $('.lists');
                 dataList.forEach(e => {
                     contentHtml.append(
                         $(`<div>`)
-<<<<<<< HEAD
-                        .attr('id', `${e._id}`)
-                        .addClass(`list`).append(
-                            $('<header>')
-                            .text(e.list).append(
-                                $('<i>')
-                                .addClass('far fa-window-close')
-                                .attr('data-id', `${e._id}`)
-                                .addClass('delete-btn'),
-                            ),
-                            $('<ul>')
-                            .addClass('locateCard'),
-                            // create a footer add a card /button
-                            $('<footer>')
-                            .text('Add a card...')
-                            .attr('id', 'clickAddList'),
-
-                        )
-=======
                             .attr('id', `${e._id}`)
                             .addClass(`list`).append(
                                 $('<header>')
@@ -46,7 +27,6 @@ $(function () {
                                     .attr('id', 'clickAddList'),
 
                             )
->>>>>>> c3761eebd0d4c2d0da473d5a6b96749acba8699e
                     )
                 })
                 contentHtml.append(
@@ -55,16 +35,10 @@ $(function () {
                         .text('Make a new list!'),
                         $('<form>').append(
                             $('<input>')
-<<<<<<< HEAD
-                            .addClass('list-input')
-                            .attr('type', "text")
-                            .attr('placeholder', "enter list title"),
-=======
                                 .addClass('list-input')
                                 .attr('type', "text")
                                 
                                 .attr('placeholder', "enter list title"),
->>>>>>> c3761eebd0d4c2d0da473d5a6b96749acba8699e
                             $('<button>')
                             .attr('id', 'add-btn')
                             .text('Add a List')
@@ -73,7 +47,7 @@ $(function () {
                 )
                 $(`.lists`).append(contentHtml);
                 $('#add-btn').on('click', addList);
-                $('.delete-btn').on('click', deleteList);
+                
 
             })
     }
@@ -92,17 +66,18 @@ $(function () {
             })
     }
 
-    const deleteList = function () {
-        const deleteID = $(event.target).attr('data-id');
+    $(document).on('click','.delete-btn',function(){
+        const deleteID = $(this).data('id');
+        console.log(deleteID)
         $.ajax({
-                url: `/api/lists/${deleteID}`,
-                method: "DELETE"
+                url: `/api/lists`,
+                method: "DELETE",
+                data: deleteID
             })
-            .then(function () {
-                $(`#${deleteID}`).remove();
-                // renderList();
+            .then(function(){
+                renderList();
             })
+    });
 
-    }
     renderList();
 });
