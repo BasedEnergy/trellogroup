9471fa27f1ad458690ef0bfa24d3cca59a998b04
FoXoLoT <KevinTheLago@hotmail.com>
2018-10-19 05:40:01 -0400

    v1.1 DragNDrop Fix

--- public/js/dragndrop.js
+++ public/js/dragndrop.js
@@ -1,9 +1,18 @@
+const dragNDropFunctions = {
+    dragNDrop: function () {
+        let cardId;
+        let firstBox;
+        let moveList;
+
+    },
+}
+
 $(document).ready(function () {
 
     $(document).on('click', '.add-btn', listFunctions.addList);
 
     $(document).on('dragstart', `.dragCard`, function () {
-        cardId = $(this).attr("data-cardName")
+        card = $(this).attr('id')
         firstBox = $(this).parent().attr("listid")
     })
 
@@ -23,12 +32,13 @@ $(document).ready(function () {
     $(document).on('drop', `.containers`, function () {
         moveList = $(this).attr("listid");
         let newData = {
-            card: cardId
+            card: card
         }
         $.ajax({ url: `/api/lists/${moveList}`, method: 'POST', data: newData })
         $.ajax({ url: `/api/lists/${firstBox}`, method: 'DELETE', data: newData })
         $('.lists').empty();
         listFunctions.renderList();
     })
+    dragNDropFunctions.dragNDrop();
 
 })
