8d7c6f86392aeb488b51c3c08a0e5132df4d43aa
FoXoLoT <KevinTheLago@hotmail.com>
2018-11-17 16:08:00 -0500

    List changes again

--- public/js/lists.js
+++ public/js/lists.js
@@ -1,19 +1,36 @@
+/**
+ * @class Card - Creates List class in global scope
+ * @constructor - takes in card data
+ * @param {string} first - takes in boardid
+ * @param {string} second - takes in listid
+ * @param {string} third - takes in list string
+ */
+class List {
+    constructor(boardid, listid, list) {
+        $(`.lists[ boardid=${boardid} ]`).append(
+            $(`<div>`).attr('listid', `${listid}`).addClass(`list`).append(
+                $('<header>').text(`${list}`).append(
+                    $('<i>').addClass('far fa-window-close delete-btn').attr('listid', `${listid}`).addClass('delete-btn'),
+                ),
+                $('<ul>').attr('listid', `${listid}`).addClass('containers'),
+                $('<footer>').text('Add a card...').addClass('containers').attr('id', 'clickAddCard').attr('listid', `${listid}`),
+            )
+        )
+    }
+}
+
 const listFunctions = {
 
+    /**
+     * @function listFunctions.renderList - Renders all Lists to the page
+     */
     renderList: function () {
         $('.lists').empty();
+
         $.ajax({ url: '/api/lists', method: 'GET' })
             .then(function (dataList) {
                 dataList.forEach(e => {
-                    $(`.lists[ boardid=${e.boardid} ]`).append(
-                        $(`<div>`).attr('listid', `${e._id}`).addClass(`list`).append(
-                            $('<header>').text(`${e.list}`).append(
-                                $('<i>').addClass('far fa-window-close delete-btn').attr('listid', `${e._id}`).addClass('delete-btn'),
-                            ),
-                            $('<ul>').attr('listid', `${e._id}`).addClass('containers'),
-                            $('<footer>').text('Add a card...').addClass('containers').attr('id', 'clickAddCard').attr('listid', `${e._id}`),
-                        )
-                    )
+                    new List(e.boardid, e._id, e.list)
                 })
                 $('.lists').append(
                     $('<div>').attr('id', 'addListBox').append(
@@ -28,26 +45,40 @@ const listFunctions = {
             })
     },
 
+    /**
+     * @function listFunctions.addList - pulls all list info
+     * @event POST - sends list to DB
+     * @function listFunctions.renderList - Re-renders Lists
+     * @function cardFunctions.renderCard - Renders Cards
+     */
     addList: function () {
         let boardid = $('.lists').attr('boardid')
         let newData = {
             list: $('#addListInput').val().trim(),
             boardid: boardid
         }
-        $.ajax({ url: '/api/lists', method: 'POST', data: newData })
+        $.ajax({
+            url: '/api/lists',
+            method: 'POST',
+            data: newData
+        })
             .then(function (e) {
                 listFunctions.renderList();
+                cardFunctions.renderCard();
             })
     },
 }
 
+/**
+ * @event listeners - ready all event listeners
+ */
 $(document).ready(function () {
 
     $(document).on('click', '#addListBox', function () {
-        $('#addListBox').attr('id', 'addingListBox').html(
+        $('#addListBox').empty()
+        $('#addListBox').attr('id', 'addingListBox').append(
             $('<input>').attr('id', 'addListInput').attr('placeholder', 'Enter list title...'),
-        );
-        $('#addingListBox').append(
+        ).append(
             $('<div>').addClass('addListBar').append(
                 $('<button>').attr('id', 'addListButton').text('Add List'),
                 $('<div>').attr('id', 'addListCancel').append(
@@ -63,10 +94,10 @@ $(document).ready(function () {
         $('.notificationsBox').remove();
     });
 
-    $(document).on('keydown', '#addListInput', function(event) {
-        if(event.which == 13) 
-           listFunctions.addList();
-      });
+    $(document).on('keydown', '#addListInput', function (event) {
+        if (event.which == 13)
+            listFunctions.addList();
+    });
 
     $(document).on('click', '#addListCancel', function () {
         var removeListBar = function () {
@@ -98,10 +129,13 @@ $(document).ready(function () {
         const deleteID = {
             _id: deletedID
         }
-        $.ajax({ url: `/api/lists`, method: "DELETE", data: deleteID })
+        $.ajax({
+            url: `/api/lists`,
+            method: "DELETE",
+            data: deleteID
+        })
             .then(function () {
                 $(target).parent().parent().remove()
             })
     });
-
 })
